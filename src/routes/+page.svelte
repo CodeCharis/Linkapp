93120e1c52cd4f7325d84c7bfaf69f4e21356062
CodeCharis <cdkumordzie@gmail.com>
2023-07-10 06:14:29 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<div class="min-h-screen flex items-center justify-center bg-slate-200">
+  <div class="bg-white w-full p-8 rounded-lg max-w-md">
+    <div class=" flex flex-col items-center mb-6">
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img
+        src="src/Images/IMG_9120.jpeg"
+        alt="my profile picture"
+        class="w-24 h-24 rounded-full object-cover"
+      />
+      <h1 class="text-2xl font-semibold my-2">Deladem Kumordzie</h1>
+      <p class="text-center my-1">
+        Challenging everything I know, unlearning & relearning⚡️ A rare breed
+        of business and technology. Business Planning || Branding || Front End
+        developer || Graphics || Entrepreneur || Interested in Venture Studios
+      </p>
+      <ul class=" space-y-4">
+        <li>
+          <a
+            href="http://twitter.com/Deladem___"
+            target="_blank"
+            class="bg-cyan-500 drop-shadow-md rounded-md px-24 py-2 text-white mt-6 w-full block text-center"
+            >Twitter</a
+          >
+          <a
+            href="https://www.linkedin.com/in/charis-deladem-kumordzie-b381bb86/"
+            target="_blank"
+            class="bg-cyan-500 drop-shadow-md rounded-md px-24 py-2 text-white mt-6 w-full block text-center"
+            >LinkedIn
+          </a>
+          <a
+            href="https://medium.com/@cdkumordzie"
+            target="_blank"
+            class="bg-cyan-500 drop-shadow-md rounded-md px-24 py-2 text-white mt-6 w-full block text-center"
+            >Medium</a
+          >
+          <a
+            href="https://deepstash.com/u/deladem_"
+            target="_blank"
+            class="bg-cyan-500 drop-shadow-md rounded-md px-24 py-2 text-white mt-6 w-full block text-center"
+            >Deepstash</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
